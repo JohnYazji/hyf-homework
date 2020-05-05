8348abda00936d1c370ca05d2a2c47d02cca32c2
JohnYazji <55900177+JohnYazji@users.noreply.github.com>
2020-05-06 00:09:43 +0200

    Update index.js

--- react/week2/src/index.js
+++ react/week2/src/index.js
@@ -1,107 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import * as serviceWorker from './serviceWorker';
-
-const TodoListItem = ({ todo, toggleComleted, deleteTodo}) => (
-<li style={{textDecoration: todo.completed ? "line-through" : "none" }}> 
-  <div>{todo.name}</div>
-  <div>{todo.description}</div>
-  <input 
-  type="checkbox"
-  checked={todo.completed}
-  onChange={toggleCompleted}
-  />
-  <button onClick={deleteTodo}>delete</button>
-</li>
-);
-
-class AddTodoForm extends React.Component {
-  state = {
-    value: "",
-  };
-
-  handleChange = (event) =>
-  this.setState({
-    value: event.target.value,
-  });
-  render() {
-    const { addTodoItem } = this.props;
-
-    return (
-      <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        this.setState({
-          value: "",
-        });
-      }}
->
-  <input
-  type="text"
-  value={this.state.value}
-  onChange={this.handleChange}
-  />
-  <button type="submit">Add Todo</button>
-        </form> 
+const TodoListItem = ({ todo, toggleCompleted, deleteTodo }) => {
+  const length = todo.length;
+  const showItems = length ? (
+    todo.map((todo) => {
+      return (
+        <div>
+          <li
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          >
+            <div>{todo.name}</div>
+            <div>{todo.description}</div>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={toggleCompleted}
+            />
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </li>
+        </div>
       );
-    }
- }
-
+    })
+  ) : (
+    <p>NO items to show</p>
+  );
+  return <div>{showItems}</div>;
+};
 
 class TodoList extends React.Component {
-  state ={ 
+  state = {
     todos: [
-    {
-      "description": "Get out of bed",
-      completed: true,
-    },
-    {
-      "description": "Brush teeth",
-      completed: false,
-    },
-    {
-      "description": "Eat breakfast",
-      completed: false,
-    },
-  ],
-};
-
-addTodoItem=(description) => {
-  const newTodo = {
-    description,
-    completed: false,
+      {
+        description: "Get out of bed",
+        completed: true,
+      },
+      {
+        description: "Brush teeth",
+        completed: false,
+      },
+      {
+        description: "Eat breakfast",
+        completed: false,
+      },
+    ],
   };
-  this.setState({
-    todos: [... this.state.todos, newTodo],
-  });
-};
 
-toggleCompleted = (currentIndex) => {
-  this.setState({ 
-   todos: this.state.todos.map((todo, index) => {
-    if (currentIndex === index) {
-      return { 
-        ...todo, 
-        completed: !todo.completed,
-      };
-  } else {
-    return todo;
+  addTodoItem = (description) => {
+    const newTodo = {
+      description,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  deleteTodo = (id) => {
+    let items = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: items,
+    });
+  };
+
+  toggleCompleted = (currentIndex) => {
+    this.setState({
+      todos: this.state.todos.map((todo, index) => {
+        if (currentIndex === index) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        } else {
+          return todo;
+        }
+      }),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TodoListItem
+          todo={this.state.todos}
+          toggleCompleted={this.toggleCompleted}
+          deleteTodo={this.deleteTodo}
+        />
+      </div>
+    );
   }
-}),
-});
-};
-
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoListItem />
+    <h1>Todo List </h1>
+    <TodoList />
   </React.StrictMode>,
-  document.getElementById('root') 
-  );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+);
