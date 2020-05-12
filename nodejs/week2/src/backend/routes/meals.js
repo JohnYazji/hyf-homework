47a47fb0408aae51930c50c136c0f79f78da27be
JohnYazji <55900177+JohnYazji@users.noreply.github.com>
2020-05-12 20:53:02 +0200

    Updated meals.js

--- nodejs/week2/src/backend/routes/meals.js
+++ nodejs/week2/src/backend/routes/meals.js
@@ -1,40 +1,56 @@
-module.exports = (req, res) =>{
+const express = require("express");
+const router = express.Router();
 
-}
 const meals = require("../data/meals.json");
+const validator = require("validator");
 
-const mealsJson = require("../data/meals");
+router.get("/meals", (request, response) => {
+  const id = request.query.id;
+  const yourMeal = meals.filter(item => item.id == id);
 
-  //Get meals that has a price smaller than maxPrice
-  const maxPrice = Number(req.query.maxPrice);
-  if (maxPrice > 0) {
-    const cheaperThan = mealsJson.filter(cor => {
-      if (Number(cor.price) < maxPrice) {
-        return cor.price;
-      }
-    });
-    res.send(cheaperThan);
-    next();
-  }
+  const maxPrice = meals.filter(item => {
+    const max = parseInt(request.query.maxPrice);
+    return max > item.price;
+  });
+
+  const mealCreatedAfter = meals.filter(item => {
+    const createdAfter = request.query.createdAfter;
+    return createdAfter < item.createdAt;
+  });
 
   const mealTitle = meals.filter(item => {
     const title = request.query.title;
     return title === item.title;
   });
 
-// Get meals that has been created after the date
-const createdAfter = parseISO(req.query.createdAfter);
-if (createdAfter > 0) {
-  const createdAfter = mealsJson.filter(cor => {
-    return parseISO(cor.createdAt) > createdAfter;
-  });
-  res.send(createdAfter);
-  next();
-};
-
-  const mealsLimited = meals.slice(item => {
-   const limit = parseInt(request.query.limit);
+  const mealsLimited = meals.filter(item => {
+    const limit = parseInt(request.query.limit);
     return meals.indexOf(item) < limit;
   });
 
-  
+  if (request.query.id) {
+    if (!validator.isNumeric(id)) {
+      response.send("Kindly make sure to search for a number as an id");
+    } else if (yourMeal.length === 0) {
+      response.send("We cant find your meal");
+    } else {
+      response.json(yourMeal);
+    }
+  }
+
+  if (request.query.limit) {
+    response.json(mealsLimited);
+  } else if (request.query.createdAfter) {
+    response.json(mealCreatedAfter);
+  } else if (request.query.title) {
+    response.json(mealTitle);
+  } else if (request.url === "/meals") {
+    response.json(meals);
+  } else if (request.query.maxPrice) {
+    response.json(maxPrice);
+  } else {
+    response.send("Make sure that you are using the correct format");
+  }
+});
+
+module.exports = router;
