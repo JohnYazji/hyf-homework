eed3736456fc4815cf3a8e903cc5bbc406986938
JohnYazji <55900177+JohnYazji@users.noreply.github.com>
2020-05-12 20:57:57 +0200

    Updated reviews.js

--- nodejs/week2/src/backend/routes/reviews.js
+++ nodejs/week2/src/backend/routes/reviews.js
@@ -1,7 +1,16 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
 const reviews = require("../data/reviews.json");
-router.get("/reviews",(request,responsse) => {
-    responsse.json(reviews);
-})
-  module.exports = router;
+
+router.get("/reviews", (request, response) => {
+  const reviewIS = reviews.filter(item => item.id == request.query.id);
+
+  if (request.url === "/reviews") {
+    response.json(reviews);
+  } else if (reviewIS.length === 0) {
+    response.send("Sorry, we did not find the review you are looking for");
+  }
+  response.json(reviewIS);
+});
+
+module.exports = router;
