2aa77c96fdce59ee57b2d6ec4dbde0d204b7780c
JohnYazji <55900177+JohnYazji@users.noreply.github.com>
2020-05-12 20:57:05 +0200

    Delete reservations.js

--- nodejs/week2/src/backend/routes/reservations.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const reservations = require("../data/reservations.json");
-router.get("/reservations",(request,responsse) => {
-    responsse.json(reservations);
-})
-  module.exports = router; 
